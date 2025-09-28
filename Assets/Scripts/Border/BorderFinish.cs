67c39b12d5b54a3970cd548fceb13f7a9e99b6fa
metinis <denis.muraska@gmail.com>
2025-09-28 10:30:17 -0400

    win screen

--- Assets/Scripts/Border/BorderFinish.cs
+++ Assets/Scripts/Border/BorderFinish.cs
@@ -2,6 +2,13 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class BorderFinish : MonoBehaviour
 {
+    public GameObject player;
+    public GameObject winText;
+    public float winScreenDuration = 4.0f;
+    private bool won = false;
+    private float duration = 0.0f;
+    private Vector3 startPosition = new Vector3(-10, -3, 0);
+    private Vector3 endPosition;
     void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
@@ -12,7 +19,22 @@ public class BorderFinish : MonoBehaviour
 
     void FinishLevel()
     {
-        Checkpoint.lastCheckpoint = new Vector3(-10, -3, 0);
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        won = true;
+        endPosition = player.transform.position;
+    }
+
+    void FixedUpdate()
+    {
+        if (won)
+        {
+            duration += Time.fixedDeltaTime;
+            if (duration >= winScreenDuration)
+            {
+                player.transform.position = endPosition;
+                winText.SetActive(true);
+                Checkpoint.lastCheckpoint = startPosition;
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+            }
+        }
     }
 }
